f2d119bd907443f2e519af27f3e6fa8cd75e8997
khoand101 <khoand101@gmail.com>
2016-12-09 16:05:20 +0700

    #CHANGE #3

--- Test/src/test/HelloWorld.java
+++ Test/src/test/HelloWorld.java
@@ -3,9 +3,6 @@ package test;
 public class HelloWorld {
 	public static void main (String[] args){
 		System.out.println("Hallo!");
-		Integer a =100, b=101;
-		System.out.println(a.compareTo(b));
-		
 	}
 
 }
