de45f04bf09cc550d5b345f7d550fa966c9923c3
khoand101 <khoand101@gmail.com>
2016-12-10 08:35:30 +0700

    Commit #2

---
+++ osg.oop1/src/osg/oop1/inheritance/Persion.java
@@ -0,0 +1,46 @@
+package osg.oop1.inheritance;
+
+/**
+ * @author KhoaND02
+ *
+ */
+public class Persion {
+	private String name;
+	private Integer yob;
+	private Integer yod;
+	
+	public Persion(String name, Integer yob, Integer yod){
+		this.name = name;
+		this.yob = yob;
+		this.yod = yod;
+	}
+	
+	public Persion (String name, Integer yob){
+		this.name = name;
+		this.yob = yob;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public Integer getYb(){
+		return yob;
+	}
+	public Integer getYod(){
+		return yod;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setYob(Integer yob){
+		this.yob = yob;
+	}
+	public void setYod(Integer yod){
+		this.yod = yod;
+	}
+	
+	
+	
+}
---
+++ osg.oop1/src/osg/oop1/inheritance/ThePsDemo.java
@@ -0,0 +1,10 @@
+package osg.oop1.inheritance;
+
+public class ThePsDemo {
+
+	public static void main(String[] args) {
+		Persion ps99 = new Persion("KhoaND ", 1989);
+		System.out.println(ps99.getName() + ps99.getYb());
+	}
+
+}
